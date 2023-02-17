8b4057f34649345a660b65cfaf0f7aa1793de677
Olawunmi07 <125666938+Olawunmi07@users.noreply.github.com>
2023-02-17 17:03:25 +0000

    Add files via upload

---
+++ ss.js
@@ -0,0 +1,14 @@
+// var olawunmi = 56
+// if (olawunmi> 52){
+//     console.log('no')
+// }
+// else if(olawunmi=56){
+//     console.log('yeah')
+// }
+// else{
+//     confirm.log('oops')
+// }
+
+for(let i=0;i<5;i++){
+    console.log(i +'. how are you')
+}
